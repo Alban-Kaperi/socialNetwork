935b3a66df1ca457c9587bdd9a9dc2673d7425e6
unknown <albanikaperi@gmail.com>
2017-02-27 15:44:30 +0100

    u shtuan usercontroller, migration, model, views dhe shenime.php

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+
+    @include('includes.header')
+    <div class="container">
+        @yield('content')
+    </div>
+
+  </body>
+</html>
